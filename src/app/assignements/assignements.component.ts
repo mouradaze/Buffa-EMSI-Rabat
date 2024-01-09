8573c9f91de61df2da972a32de3f5497c69382b5
mouradaze <mouradaze10@gmail.com>
2024-01-09 18:08:49 +0100

    forms

--- src/app/assignements/assignements.component.ts
+++ src/app/assignements/assignements.component.ts
@@ -1,16 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RenduDirective } from '../shared/rendu.directive';
+
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 
 
 @Component({
   selector: 'app-assignements',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RenduDirective,FormsModule,MatButtonModule,
+    MatDatepickerModule,MatFormFieldModule,MatInputModule,MatNativeDateModule],
   templateUrl: './assignements.component.html',
   styleUrl: './assignements.component.css'
 })
-export class AssignementsComponent {
+export class AssignementsComponent implements OnInit {
+  nomDevoir = ""
+  datePicked!:Date
+  onSubmit(event:any){
+    console.log(this.nomDevoir+" d : "+this.datePicked)
+
+  }
+  ngOnInit():void{
+    setTimeout(()=>{
+      this.isActive = true
+    },2000)
+  }
   title = "Assignement component is working !!!"
+  isActive = false
   assignements = [
     {
       nom:'Name 1',
